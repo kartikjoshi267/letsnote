d49bc801165dd0ea542d584b661ca77c59093c54
Kartik Joshi <96565730+kartikjoshi267@users.noreply.github.com>
2022-08-10 16:21:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/api/notes', notes);
 
 // Heroku
 __dirname = path.resolve();
-if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV !== "production"){
     app.use(express.static(path.join(__dirname, '/client/build')))
 
     app.get('*', (req, res) => {
